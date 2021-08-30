d0dccd2f30ff9c9c06240af9e061a3cfa6b66fb1
leejongwoo <leejongwoo.dev@gmail.com>
2021-08-30 22:29:30 +0900

    initial commit

---
+++ src/main/java/com/sec/form/SecFormApplication.java
@@ -0,0 +1,13 @@
+package com.sec.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sec/form/SecFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
