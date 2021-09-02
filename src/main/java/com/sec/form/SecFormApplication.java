2c2784a836ae3360d07630c664ccada83ecd55e2
jongwoo <leejongwoo.dev@gmail.com>
2021-09-02 21:19:20 +0900

    맵퍼 설정

--- src/main/java/com/sec/form/SecFormApplication.java
+++ src/main/java/com/sec/form/SecFormApplication.java
@@ -1,8 +1,10 @@
 package com.sec.form;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = "com.sec.form.mapper")
 @SpringBootApplication
 public class SecFormApplication {
 
