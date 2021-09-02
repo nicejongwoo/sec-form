5fcc65c2015ffec6efbe011c92442aab8cb2b72f
jongwoo <leejongwoo.dev@gmail.com>
2021-09-02 23:13:15 +0900

    코드그룹 등록 폼

--- src/main/java/com/sec/form/controller/CodeGroupController.java
+++ src/main/java/com/sec/form/controller/CodeGroupController.java
@@ -1,8 +1,11 @@
 package com.sec.form.controller;
 
+import com.sec.form.domain.CodeGroup;
 import com.sec.form.service.CodeGroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CodeGroupController {
 
     private final CodeGroupService codeGroupService;
+
+    //등록폼
+    @GetMapping("/register")
+    public void registerForm(Model model) {
+        model.addAttribute("codeGroup", new CodeGroup());
+    }
 }
