1f3cbefb901c67178d34018d3a2ef2738712cccf
jongwoo <leejongwoo.dev@gmail.com>
2021-09-04 21:34:28 +0900

    목록 조회 기능 service, mapper 테스트

--- src/test/java/com/sec/form/mapper/CodeGroupMapperTest.java
+++ src/test/java/com/sec/form/mapper/CodeGroupMapperTest.java
@@ -3,13 +3,10 @@ package com.sec.form.mapper;
 import com.sec.form.domain.CodeGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,6 +26,17 @@ class CodeGroupMapperTest {
         codeGroupMapper.create(codeGroup);
         CodeGroup savedData = codeGroupMapper.read(codeGroup.getGroupCode());
         assertEquals(codeGroup.getGroupCode(), savedData.getGroupCode());
+    }
+
+    @DisplayName("list_mapper_test")
+    @Test
+    void listMapperTest(){
+        CodeGroup codeGroup = new CodeGroup();
+        codeGroup.setGroupCode("001");
+        codeGroup.setGroupName("codename");
+        codeGroupMapper.create(codeGroup);
+        List<CodeGroup> list = codeGroupMapper.list();
+        assertEquals(1, list.size());
     }
 
 }
--- src/test/java/com/sec/form/service/CodeGroupServiceImplTest.java
+++ src/test/java/com/sec/form/service/CodeGroupServiceImplTest.java
@@ -2,7 +2,6 @@ package com.sec.form.service;
 
 import com.sec.form.domain.CodeGroup;
 import com.sec.form.mapper.CodeGroupMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -31,6 +35,32 @@ class CodeGroupServiceImplTest {
         mockCodeGroup.setGroupName("codename");
         codeGroupService.register(mockCodeGroup);
         verify(codeGroupMapper, times(1)).create(mockCodeGroup);
+    }
+
+
+    @DisplayName("codeGroup list service test")
+    @Test
+    void listTest(){
+        List<CodeGroup> mockList = new ArrayList<>();
+
+        CodeGroup mockCodeGroup = new CodeGroup();
+        mockCodeGroup.setGroupCode("001");
+        mockCodeGroup.setGroupName("codename001");
+        CodeGroup mockCodeGroup2 = new CodeGroup();
+        mockCodeGroup2.setGroupCode("002");
+        mockCodeGroup2.setGroupName("codename002");
+
+        mockList.add(0, mockCodeGroup);
+        mockList.add(1, mockCodeGroup2);
+
+        //의존하는 mock객체에서 해당 리스트를 받아 왔다고 가정하고 값을 정함
+        given(codeGroupMapper.list()).willReturn(mockList);
+
+        List<CodeGroup> list = codeGroupService.list();
+        CodeGroup codeGroup = list.get(0);
+        assertEquals("codename001", codeGroup.getGroupName());
+        assertEquals(2, list.size());
+
     }
 
 }
