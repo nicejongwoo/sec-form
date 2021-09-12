9929555be2095275e9a6ac88f376f80e1ad629a1
jongwoo <leejongwoo.dev@gmail.com>
2021-09-12 19:37:43 +0900

    코드관리 등록화면

--- src/main/java/com/sec/form/controller/CodeDetailController.java
+++ src/main/java/com/sec/form/controller/CodeDetailController.java
@@ -1,9 +1,12 @@
 package com.sec.form.controller;
 
+import com.sec.form.domain.CodeDetail;
 import com.sec.form.service.CodeDetailService;
 import com.sec.form.service.CodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -13,5 +16,11 @@ public class CodeDetailController {
 
     private final CodeDetailService codeDetailService;
     private final CodeService codeService;
+
+    @GetMapping("/register")
+    public void registerForm(Model model) {
+        model.addAttribute(new CodeDetail());
+        model.addAttribute("groupCodeList", codeService.getCodeGroupList());
+    }
 
 }
