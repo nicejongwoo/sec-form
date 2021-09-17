2a9ec82ba41271ec9741e1952b516a4044e92c3e
jongwoo <leejongwoo.dev@gmail.com>
2021-09-17 23:53:40 +0900

    코드 테스트 데이터 삽입

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO code_group (group_code, group_name) VALUES ('A01', 'job');
 INSERT INTO code_group (group_code, group_name) VALUES ('A02', 'hobby');
+
+INSERT INTO code_detail (group_code, code_value, code_name, sort_seq, use_yn) VALUES ('A01', '001', 'A01001', 0, 'Y');
+INSERT INTO code_detail (group_code, code_value, code_name, sort_seq, use_yn) VALUES ('A01', '002', 'A01002', 1, 'Y');
+INSERT INTO code_detail (group_code, code_value, code_name, sort_seq, use_yn) VALUES ('A02', '001', 'A02001', 0, 'Y');
