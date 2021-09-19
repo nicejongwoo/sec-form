948e14b56438eb82ced4b114abafa1ee6c022dfc
jongwoo <leejongwoo.dev@gmail.com>
2021-09-19 16:56:45 +0900

    로그인 페이지

---
+++ src/main/java/com/sec/form/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.sec.form.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/auth")
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm(String error, String logout, Model model) {
+        if (error != null) {
+            model.addAttribute("error", "Login Error");
+        }
+        if (logout != null) {
+            model.addAttribute("logout", "Logout");
+        }
+        return "auth/loginForm";
+    }
+
+}
