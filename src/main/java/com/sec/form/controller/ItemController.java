c70c09de49bc74bfc86f8d9c346ce8bfd3cf2431
jongwoo <leejongwoo.dev@gmail.com>
2021-09-19 23:54:32 +0900

    item 등록 화면

--- src/main/java/com/sec/form/controller/ItemController.java
+++ src/main/java/com/sec/form/controller/ItemController.java
@@ -1,8 +1,12 @@
 package com.sec.form.controller;
 
+import com.sec.form.domain.Item;
+import com.sec.form.prop.ShopProperties;
 import com.sec.form.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -11,5 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ItemController {
 
     private ItemService itemService;
+    private final ShopProperties shopProperties;
+
+    @GetMapping("/register")
+    public void registerForm(Model model) {
+        model.addAttribute(new Item());
+    }
 
 }
