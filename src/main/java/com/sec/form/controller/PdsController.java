1ddf81481497fc5bd8aa6e1578736e5194b0aba3
jongwoo <leejongwoo.dev@gmail.com>
2021-09-20 19:29:03 +0900

    자료실 등록 화면

--- src/main/java/com/sec/form/controller/PdsController.java
+++ src/main/java/com/sec/form/controller/PdsController.java
@@ -1,9 +1,12 @@
 package com.sec.form.controller;
 
+import com.sec.form.domain.Pds;
 import com.sec.form.prop.ShopProperties;
 import com.sec.form.service.PdsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -13,5 +16,11 @@ public class PdsController {
 
     private final PdsService pdsService;
     private final ShopProperties shopProperties;
+
+    @GetMapping("/register")
+    public String registerForm(Model model) {
+        model.addAttribute(new Pds());
+        return "pds/register";
+    }
 
 }
