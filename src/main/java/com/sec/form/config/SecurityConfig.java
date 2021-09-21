54faae37d3200b342254262ff0e757669409486f
jongwoo <leejongwoo.dev@gmail.com>
2021-09-21 22:11:03 +0900

    인메모리 사용자 설정

--- src/main/java/com/sec/form/config/SecurityConfig.java
+++ src/main/java/com/sec/form/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sec.form.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,10 +9,44 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("123").roles("MEMBER")
+//                .and()
+//                .withUser("manager").password("123").roles("MEMBER", "ADMIN")
+//                .and()
+//                .withUser("admin").password("123").roles("ADMIN");
+//    }
+
+//    //UserDetailsService 재정의
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//        UserDetails user = users.username("user")
+//                .password("123")
+//                .roles("MEMBER")
+//                .build();
+//
+//        UserDetails manager = users.username("manager")
+//                .password("123")
+//                .roles("MEMBER", "ADMIN")
+//                .build();
+//
+//        UserDetails admin = users.username("admin")
+//                .password("123")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, manager, admin);
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-//        http.formLogin();
+//        http.httpBasic();
+        http.formLogin();
         http.authorizeRequests()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .anyRequest()
