b97443ae1e5dcf0f256b96074d214b3402dbbc65
jongwoo <leejongwoo.dev@gmail.com>
2021-09-21 23:04:00 +0900

    bcrypt 암호화

--- src/main/java/com/sec/form/config/SecurityConfig.java
+++ src/main/java/com/sec/form/config/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.sec.form.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password("123").roles("MEMBER")
-//                .and()
-//                .withUser("manager").password("123").roles("MEMBER", "ADMIN")
-//                .and()
-//                .withUser("admin").password("123").roles("ADMIN");
-//    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("123")).roles("MEMBER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("123")).roles("MEMBER", "ADMIN")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("123")).roles("ADMIN");
+    }
 
 //    //UserDetailsService 재정의
 //    @Bean
@@ -42,6 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //        return new InMemoryUserDetailsManager(user, manager, admin);
 //    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
